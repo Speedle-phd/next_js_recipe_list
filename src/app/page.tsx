3a681e7fd5095e42788d3a6b289d4cbef1de8f9b
Emanuel Eirich <shanks-@gmx.de>
2023-11-20 22:15:34 +0100

    whatever

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/lib/db"
 export default async function Home() {
    const users = await prisma.user.findMany()
    console.log(users)
+   console.log("some change")
   return (
     <main>
       {JSON.stringify(users)}
